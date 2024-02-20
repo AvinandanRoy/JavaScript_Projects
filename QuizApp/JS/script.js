eee102d2555bcb01ef7620259c60feab7d0b4326
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-02-20 14:32:11 +0600

    css and html are added for this project

---
+++ QuizApp/JS/script.js
@@ -0,0 +1,20 @@
+// set the ques
+
+const question = [
+    {question : "Which is the largest animal of the world?",
+    answer : [
+        {text : "Shark",correct : false},
+        {text : "Tiger",correct: false},
+        {text : "Blue whale",correct: true},
+        {text : "Elephant",correct: false}
+    ]
+    },
+    {question : "Which is the smallest Continent of the world?",
+    answer : [
+        {text : "Asia",correct : false},
+        {text : "Australia",correct: true},
+        {text : "Arctic",correct: false},
+        {text : "Africa",correct: false}
+    ]
+    }
+];
