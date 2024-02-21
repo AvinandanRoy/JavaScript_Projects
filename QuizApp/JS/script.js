f53872255f362de84011187b060c46862bd2cccd
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-02-21 18:28:01 +0600

    Quiz app completed

--- QuizApp/JS/script.js
+++ QuizApp/JS/script.js
@@ -1,6 +1,6 @@
 // set the ques
 
-const question = [
+const questions = [
     {question : "Which is the largest animal of the world?",
     answer : [
         {text : "Shark",correct : false},
@@ -18,3 +18,94 @@ const question = [
     ]
     }
 ];
+
+const questionElement = document.getElementById("question");
+
+const answerButtons = document.getElementById("answerButtons");
+
+const nextButton = document.getElementById("nextButton");
+
+let currentQuestionIndex = 0;
+let score = 0 ;
+
+
+function startQuiz(){
+    currentQuestionIndex =0 ;
+    score=0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1 ;
+    questionElement.innerHTML = questionNo+". "+currentQuestion.question;
+
+    currentQuestion.answer.forEach(answer=>{
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct ==="true";
+    if(isCorrect){
+        selectBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button=>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}`;
+    questionElement.style.margin= "0 auto";
+    nextButton.innerHTML ="Play Again";
+    nextButton.style.display = "block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+})
+
+startQuiz();
+console.log(questions.length);
